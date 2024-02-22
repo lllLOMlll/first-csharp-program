3bc26a0e878b23751450b44d54c8672db7628ef2
LOMDataFlowSolution <lom@dataflowsolution.com>
2024-02-22 16:06:27 -0500

    Constructors halfway done

--- Book.cs
+++ Book.cs
@@ -11,5 +11,14 @@ namespace MyFirstCSharpProgram
         public string title;
         public string author;
         public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -238,23 +238,31 @@ namespace MyFirstCSharpProgram
 
             // *********************************************************************
             // CLASSES AND OBJECTS
-            Book book1 = new Book();
-            book1.title = "Harry Potter";
-            book1.author = "JK Rowling";
-            book1.pages = 400;
+            //Book book1 = new Book("Mike");
+            //book1.title = "Harry Potter";
+            //book1.author = "JK Rowling";
+            //book1.pages = 400;
 
-            Book book2 = new Book();
-            book2.title = "Lord of the Rings";
-            book2.author = "Tolkein";
-            book2.pages = 700;
+            //Book book2 = new Book("John");
+            //book2.title = "Lord of the Rings";
+            //book2.author = "Tolkein";
+            //book2.pages = 700;
 
+            //Console.WriteLine(book1.title);
+            //Console.WriteLine(book2.author);
+
+            //Console.ReadLine();
+
+            // *********************************************************************
+            // CONSTRUCTORS
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+        
+            Book book2 = new Book("Lord of the Rings", "Tolkien", 700);
+    
             Console.WriteLine(book1.title);
             Console.WriteLine(book2.author);
 
             Console.ReadLine();
-
-
-
 
 
 
