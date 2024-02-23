f376fced705dfad281939c58708fe5da06ec5587
LOMDataFlowSolution <lom@dataflowsolution.com>
2024-02-23 10:06:11 -0500

    Static Methods and Classes - UsefulTools

--- Program.cs
+++ Program.cs
@@ -295,15 +295,24 @@ namespace MyFirstCSharpProgram
 
             // *********************************************************************
             // STATIC CLASS ATTRIBUTES
-            Song holiday = new Song("Holiday", "Green Day", 200);
-            // Console.WriteLine(Song.songCount);
-            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
-            // Console.WriteLine(Song.songCount);
-            Console.WriteLine(holiday.getSongCount());
-            Console.WriteLine(Song.songCount);
-        
-            
+            //Song holiday = new Song("Holiday", "Green Day", 200);
+            //// Console.WriteLine(Song.songCount);
+            //Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+            //// Console.WriteLine(Song.songCount);
+            //Console.WriteLine(holiday.getSongCount());
+            //Console.WriteLine(Song.songCount);
+
+
+            //Console.ReadLine();
+
+
+
+            // *********************************************************************
+            // STATIC METHODS AND CLASSES
+            UsefulTools.SayHi("Louis");
             Console.ReadLine();
+
+
 
 
 
